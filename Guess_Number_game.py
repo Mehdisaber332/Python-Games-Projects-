ae75aaedea31a3fd74a212c7247781ea79f61bcd
Mehdisaber332 <mehdisaber332@gmail.com>
2023-09-29 23:59:40 +0100

    Try this guessing game

--- Guess_Number_game.py
+++ Guess_Number_game.py
@@ -11,18 +11,24 @@ if top_range.isdigit():
         quit()
 else:
     print("Please enter a valid number") 
-
+    
 random_number = rm.randint(0, top_range)
-
+guesses = 0 
 while True:
+    guesses += 1 
     user_guess = input("Make a guess: ")
     if user_guess.isdigit():
         user_guess = int(user_guess)
     else: 
         print("Please enter a valid guess number!")
         continue
+
     if user_guess == top_range:
         print("Good job, you got it!")
         break
+    elif user_guess < top_range:
+        print("No, its more bigger")
     else:
-        print("You got it wrong")
+        print("No, more lower")
+
+print(f"You got it in {guesses} guesses")
